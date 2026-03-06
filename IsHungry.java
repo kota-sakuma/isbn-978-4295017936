4bd602912d78f2385c52efd22be7b6c837da81b2
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-06 23:56:28 +0900

    feat: add IsHungry.java

---
+++ IsHungry.java
@@ -0,0 +1,19 @@
+public class IsHungry {
+
+  public static void main(String[] args) {
+
+    int isHungry = 0;
+    String food = "curry";
+
+    System.out.println("Hi!");
+
+    if (isHungry == 0) {
+      System.out.println("I'm full.");
+    } else {
+      System.out.println("I'm hungry.");
+      System.out.println("I'll eat " + food + ".");
+    }
+
+    System.out.println("Goodbye!");
+  }
+}
