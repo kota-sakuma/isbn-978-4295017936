ba6d4a3a966fffc0d97f8bffad13a83bebb8e757
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-07 12:33:56 +0900

    feat: add Mondokoro.java

---
+++ Mondokoro.java
@@ -0,0 +1,12 @@
+import comment.Kouhan;
+import comment.Zenhan;
+
+public class Mondokoro {
+
+  public static void main(String[] args) {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    Kouhan.callDeae();
+    Kouhan.showMondokoro();
+  }
+}
