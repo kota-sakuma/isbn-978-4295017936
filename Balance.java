a2959f64743e8be4d776796ffba59407a760a748
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-07 11:35:37 +0900

    feat: add Balance.java

---
+++ Balance.java
@@ -0,0 +1,13 @@
+public class Balance {
+
+  public static void main(String[] args) {
+    int[] moneyList = {121902, 8302, 55100};
+    for (int i = 0; i < moneyList.length; i++) {
+      System.out.println(moneyList[i]);
+    }
+
+    for (int balance : moneyList) {
+      System.out.println(balance);
+    }
+  }
+}
