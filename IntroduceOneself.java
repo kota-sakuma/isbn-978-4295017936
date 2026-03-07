8f2500156b0ec73c98aa8d1b5caed801c72378d0
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-07 12:02:29 +0900

    feat: add IntroduceOneself.java

---
+++ IntroduceOneself.java
@@ -0,0 +1,18 @@
+public class IntroduceOneself {
+
+  public static void main(String[] args) {
+    introduceOneself();
+  }
+
+  public static void introduceOneself() {
+    String name = "Kota";
+    int age = 20;
+    double height = 200.0;
+    String zodiac = "Leo";
+
+    System.out.println("My name is " + name + ".");
+    System.out.println("I'm " + age + " years old.");
+    System.out.println("I'm " + height + " cm tall.");
+    System.out.println("My zodiac is " + zodiac + ".");
+  }
+}
